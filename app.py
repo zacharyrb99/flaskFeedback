a0c9cefc2e7888e1ea31936009f2121715eba7f4
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-06-30 07:30:13 -0400

    add heroku environ vars

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from sqlalchemy.sql import exists
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User, Feedback
 from forms import FeedbackForm, RegisterForm, LoginForm
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///feedback_db') #'postgresql:///feedback_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'password'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret12345')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
